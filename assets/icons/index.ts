1aae96b6f1f6e1021cd04dfc1f7f6bf8f77fc745
josemak25 <amakirij@gmail.com>
2020-09-02 19:38:19 +0100

    add default export for app icons

---
+++ assets/icons/index.ts
@@ -0,0 +1,11 @@
+const APP_ICONS = {
+  alarm_active: require("../images/alarm_active.png"),
+  alarm_inactive: require("../images/alarm_inactive.png"),
+  list_active: require("../images/list_active.png"),
+  list_inactive: require("../images/list_inactive.png"),
+  settings_active: require("../images/settings_active.png"),
+  settings_inactive: require("../images/settings_inactive.png"),
+  clock_hands: require("../images/clock_hands.png"),
+};
+
+export default APP_ICONS;
