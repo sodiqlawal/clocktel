a92e86ebfa4fea222b727edc352c4262d1dd4b6a
josemak25 <amakirij@gmail.com>
2020-09-02 20:27:04 +0100

    add clock list and setting screens

--- src/screens/setting/styles.ts
+++ src/screens/setting/styles.ts
@@ -1,8 +1,8 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.WHITE};
+  background-color: ${({ theme }) => theme.colors.PRIMARY};
 `;
