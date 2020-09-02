04bede59090a8c0157cb69ea96b4a41d38b9d0bc
josemak25 <amakirij@gmail.com>
2020-09-02 20:15:45 +0100

    add app resource loader

--- src/libs/loadResources/index.tsx
+++ src/libs/loadResources/index.tsx
@@ -1,13 +1,13 @@
-import { Image } from 'react-native';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
+import { Image } from "react-native";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
 
 type FontType = { [name: string]: Font.FontSource };
 
 export default async function LoadAppReasources() {
   const cacheImages = (images: number[]) => {
     return images.map((image) => {
-      return typeof image === 'string'
+      return typeof image === "string"
         ? Image.prefetch(image)
         : Asset.fromModule(image).downloadAsync();
     });
@@ -18,18 +18,24 @@ export default async function LoadAppReasources() {
   };
 
   const imageAssets = cacheImages([
-    // require('../../../assets/images/splash.png'),
+    require("../../../assets/images/alarm_active.png"),
+    require("../../../assets/images/alarm_inactive.png"),
+    require("../../../assets/images/list_active.png"),
+    require("../../../assets/images/list_inactive.png"),
+    require("../../../assets/images/settings_active.png"),
+    require("../../../assets/images/settings_inactive.png"),
+    require("../../../assets/images/clock_hands.png"),
   ]);
 
   const fontAssets = cacheFonts([
     {
-      aileronRegular: require('../../../assets/fonts/aileronRegular.otf')
+      aileronRegular: require("../../../assets/fonts/aileronRegular.otf"),
     },
     {
-      aileronSemiBold: require('../../../assets/fonts/aileronSemiBold.otf')
+      aileronSemiBold: require("../../../assets/fonts/aileronSemiBold.otf"),
     },
     {
-      aileronBold: require('../../../assets/fonts/aileronBold.otf')
+      aileronBold: require("../../../assets/fonts/aileronBold.otf"),
     },
   ]);
 
