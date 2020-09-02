fad615c3546f7040f5c6e04296e6422f00b431fd
josemak25 <amakirij@gmail.com>
2020-09-02 20:33:16 +0100

    add app global light and dark mode theme

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -2,14 +2,28 @@ import 'styled-components';
 import { configureFonts, DefaultTheme } from 'react-native-paper';
 
 // All app colors
-enum COLORS {
-  WHITE = '#F7F9FC',
+enum DARK_MODE_COLORS {
+  WHITE = '#FFFFFF',
+  PRIMARY = '#142550',
+  SECONDARY = '#D92D7B',
   DARK_BLUE = '#142550',
-  PRIMARY = '#D92D7B',
-  SECONDARY = '#E7EEFB',
+  LIGHT_GREY = '#F7F9FC',
+  CLOCK_INSET = '#22335D',
+  PRIMARY_TEXT = '#FFFFFF',
+  SECONDARY_TEXT = '#B0B4BD',
+  ICON_TINT_COLOR = '#FFFFFF'
+}
+
+enum LIGHT_MODE_COLORS {
+  WHITE = '#FFFFFF',
+  PRIMARY = '#EDF1FB',
+  SECONDARY = '#D92D7B',
+  DARK_BLUE = '#142550',
+  LIGHT_GREY = '#F7F9FC',
+  CLOCK_INSET = '#E7EEFB',
   PRIMARY_TEXT = '#142550',
   SECONDARY_TEXT = '#B0B4BD',
-  ACTION = '#D92D7B'
+  ICON_TINT_COLOR = '#142550'
 }
 
 // All app font sizes
@@ -26,23 +40,25 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     // All Global App Colors
     colors: {
-      WHITE: string,
-      DARK_BLUE:string,
-      PRIMARY:string,
-      SECONDARY:string,
-      PRIMARY_TEXT:string,
-      SECONDARY_TEXT:string,
-      ACTION:string
+      WHITE: string;
+      PRIMARY: string;
+      DARK_BLUE: string;
+      SECONDARY: string;
+      LIGHT_GREY: string;
+      CLOCK_INSET: string;
+      PRIMARY_TEXT: string;
+      SECONDARY_TEXT: string;
+      ICON_TINT_COLOR: string;
     };
 
     // All Global App Font Sizes
     fonts: {
       SMALL_SIZE: number;
-      MEDIUM_SIZE: number;
       LARGE_SIZE: number;
-      AILERON_REGULAR: string,
-      AILERON_SEMI_BOLD: string,
-      AILERON_BOLD: string
+      MEDIUM_SIZE: number;
+      AILERON_BOLD: string;
+      AILERON_REGULAR: string;
+      AILERON_SEMI_BOLD: string;
     };
   }
 }
@@ -76,16 +92,19 @@ export const paperTheme = {
   roundness: 2,
   colors: {
     ...DefaultTheme.colors,
-    primary: COLORS.PRIMARY,
-    accent: COLORS.SECONDARY,
-    background: COLORS.WHITE,
-    surface: COLORS.WHITE,
-    text: COLORS.PRIMARY_TEXT,
-    disabled: COLORS.SECONDARY_TEXT
+    surface: LIGHT_MODE_COLORS.WHITE,
+    primary: LIGHT_MODE_COLORS.PRIMARY,
+    accent: LIGHT_MODE_COLORS.SECONDARY,
+    background: LIGHT_MODE_COLORS.WHITE,
+    text: LIGHT_MODE_COLORS.PRIMARY_TEXT
   },
   // @ts-ignore
   fonts: configureFonts(paperFontConfig)
 };
 
 // App theme
-export const styledComponentTheme = { colors: COLORS, fonts: FONTS };
+export const styledComponentTheme = {
+  fonts: FONTS,
+  DARK_MODE_COLORS,
+  LIGHT_MODE_COLORS
+};
