25fb3755b9448d6d694acf4c408991940d39ff7f
josemak25 <amakirij@gmail.com>
2020-09-02 20:33:30 +0100

    add app global theme as HOC

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,20 +1,24 @@
 import React, { FunctionComponent, useContext } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
-import {
-  ThemeProvider as Provider,
-  ThemeContext
-} from 'styled-components';
+import { ThemeProvider as Provider, ThemeContext } from 'styled-components';
 import { AntDesign } from '@expo/vector-icons';
-
+import { StatusBar } from 'expo-status-bar';
+import { useSelector } from '../store';
 import { styledComponentTheme, paperTheme } from './types';
 
 const ThemeProvider: FunctionComponent = ({ children }) => {
+  const { status } = useSelector(({ dlsState }) => dlsState);
+
+  const { DARK_MODE_COLORS, LIGHT_MODE_COLORS, fonts } = styledComponentTheme;
+  const colors = status ? DARK_MODE_COLORS : LIGHT_MODE_COLORS;
+
   return (
     <PaperProvider
       theme={paperTheme}
       settings={{ icon: (props) => <AntDesign {...props} /> }}
     >
-      <Provider theme={styledComponentTheme}>{children}</Provider>
+      <StatusBar translucent animated style={status ? 'light' : 'dark'} />
+      <Provider theme={{ fonts, colors }}>{children}</Provider>
     </PaperProvider>
   );
 };
@@ -22,4 +26,3 @@ const ThemeProvider: FunctionComponent = ({ children }) => {
 export const useAppTheme = () => useContext(ThemeContext);
 
 export default ThemeProvider;
-
